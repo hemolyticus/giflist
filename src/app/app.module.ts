ff11854339fbd9165cd7dce2c97ef25d6a397582
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-01-01 14:22:51 +1300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { Storage } from '@ionic/storage';
+import { SettingsPage} from '../pages/settings/settings';
+import { Reddit } from '../providers/reddit';
+import { Data } from '../providers/data';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    SettingsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    SettingsPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, Storage, Data, Reddit]
+})
+export class AppModule {}
