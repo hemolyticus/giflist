cb57d1ad037ae37980b9708766b7aad248d45f37
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-01-01 15:13:43 +1300

    Imported pages etc. Added Parameters to the constructors Created methods

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,84 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { ModalController, Platform } from 'ionic-angular';
+import { Keyboard } from 'ionic-native';
+import { SettingsPage } from '../settings/settings';
+import { Data } from '../../providers/data';
+import { Reddit } from '../../providers/reddit';
+import { FormControl } from '@angular/forms';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    //Variables
+    subredditValue:string;
+
+    //Constructor
+    constructor(public dataService: Data, public redditService: Reddit, public modalCtrl: ModalController, public platform:Platform)
+    {
     
-  }
+    }
+
+    ionViewDidLoad()
+    {
+
+        this.platform.ready().then(()=>
+        {
+            this.loadSettings();
+        });
+    }
+
+
+    //Methods
+
+    loadSettings():void
+    {
+
+        console.log("TODO: Implement loadSettings()");
+
+    }
+
+    showComments(post):void
+    {
+
+        console.log("TODO: Implement showComments()");
+
+    }
+
+    openSettings(): void
+    {
+
+        console.log("TODO: Implement openSettings()");
+
+    }
+
+    playVideo(e, post):void
+    {
+
+        console.log("TODO: Implement playVideo()");
+
+    }
+
+    changeSubreddit(): void
+    {
+
+        console.log("TODO: Implement changeSubreddit()");
+
+    }
+
+    loadMore(): void
+    {
+        console.log("TODO: Implement loadMore()");
+
+    }
+
+
 
 }
